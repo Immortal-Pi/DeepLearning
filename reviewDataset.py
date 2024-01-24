459f96a6cc7c7c8dba2fe38e724aa51086310517
unknown <26.amruth@gmail.com>
2024-01-24 22:32:18 +0530

    augmentation

--- reviewDataset.py
+++ reviewDataset.py
@@ -1,3 +1,5 @@
+import os
+
 import tensorflow as tf
 import cv2
 import json
@@ -9,7 +11,7 @@ from matplotlib import pyplot as plt
 #     tf.config.experimental.set_memory_growth(gpu,True)
 # tf.config.list_physical_devices('GPU')
 
-images=tf.data.Dataset.list_files('data\\images\\*.jpg',shuffle=False)
+# images=tf.data.Dataset.list_files('data\\images\\*.jpg',shuffle=False)
 # print(images.as_numpy_iterator().next())
 
 def load_image(x):
@@ -18,14 +20,26 @@ def load_image(x):
     return img
 
 
-images = images.map(load_image)
-# print(images.as_numpy_iterator().next())
-image_generator=images.batch(4).as_numpy_iterator()
-plot_images=image_generator.next()
+# images = images.map(load_image)
+# # print(images.as_numpy_iterator().next())
+# image_generator=images.batch(4).as_numpy_iterator()
+# plot_images=image_generator.next()
 
-fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-for idx, image in enumerate(plot_images):
-    ax[idx].imshow(image)
-plt.show()
+# fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+# for idx, image in enumerate(plot_images):
+#     ax[idx].imshow(image)
+# plt.show()
+
+
+# move matching labels
+
+for folder in ['train','test','val']:
+    for file in os.listdir(os.path.join('data',folder,'images')):
+        filename=file.split('.')[0]+'.json'
+        existingPath=os.path.join('data','labels',filename)
+        newPath = os.path.join('data', folder, 'labels', filename)
+        if os.path.exists(existingPath):
+
+            os.replace(existingPath,newPath)
 
 
