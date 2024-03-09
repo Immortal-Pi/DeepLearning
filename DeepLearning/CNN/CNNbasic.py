dc9291728d1c236a8aa3c3d25d7c922a8df98672
unknown <26.amruth@gmail.com>
2024-03-09 11:26:38 +0530

    CNN architecture

--- DeepLearning/CNN/CNNbasic.py
+++ DeepLearning/CNN/CNNbasic.py
@@ -4,11 +4,14 @@
 #take the image data and normalize the data
 #label encode
 #define CNN
+import warnings
+
 import numpy as np
 #prediction and actual accuracy
 #Confusion matrix visulization
 
 
+import tensorflow as tf
 import pandas as pd
 import os
 from PIL import Image
@@ -16,13 +19,26 @@ import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-
-
-
+from sklearn.preprocessing import LabelEncoder
+import keras.layers
+from keras.utils.np_utils import to_categorical
+from keras import Sequential
+from keras.regularizers import L2
 #get data
 label_data=pd.read_csv('datasets/cifar10Labels.csv',index_col=0)
-print(label_data)
+import keras.callbacks
+# from warnings
+from mlxtend.evaluate import confusion_matrix,scoring
+from mlxtend.plotting import plot_confusion_matrix
+from skimage.transform import resize
+from  vis.visualization import visualize_cam,visualize_saliency,overlay
+from vis.utils import utils
 
+
+
+
+
+warnings.filterwarnings('ignore')
 
 
 #show random picture
@@ -45,9 +61,111 @@ for i in ytrain.index:
     temp.append(img)
 
 xtrain=np.stack(temp)
-print(xtrain)
-
+print(xtrain.shape)
+temp=[]
 for i in ytest.index:
     img_path=os.path.join('datasets/cifar10',f'{i}.png')
+    img=np.array(imageio.imread(img_path)).astype('float32')
+    temp.append(img)
+xtest=np.stack(temp)
+print(xtest.shape)
+
+#normalize data
+xtrain=xtrain/255.
+xtest=xtest/255.
+
+
+#encode
+lb=LabelEncoder()
+
+ytrain=lb.fit_transform(ytrain)
+ytrain_norm=to_categorical(ytrain)
+
+#
+# print(ytest)
+ytest=lb.fit_transform(ytest)
+ytest_norm=to_categorical(ytest)
+print(ytrain_norm.shape,ytest_norm.shape)
+
+
+#build CNN network
+num_classes=10
+
+model=Sequential([
+    keras.layers.Conv2D(filters=32,kernel_size=(3,3),strides=1,padding='same',activation='relu',kernel_regularizer=L2(0.01),input_shape=(32,32,3),name='conv1'),
+    keras.layers.Normalization(name='norm1'),
+    keras.layers.MaxPool2D(pool_size=(2,2),name='maxpool1'),
+    keras.layers.Conv2D(filters=64,kernel_size=(3,3),strides=1,padding='same',activation='relu',kernel_regularizer=L2(0.01),input_shape=(32,32,3),name='conv2'),
+    keras.layers.Normalization(name='norm2'),
+    keras.layers.MaxPool2D(pool_size=(2,2),name='maxpool2'),
+    keras.layers.Flatten(name='flat'),
+    keras.layers.Dense(num_classes,activation='softmax',name='pred_values'),
+])
+
+model.compile(loss='categorical_crossentropy',optimizer=tf.optimizers.Adam(),metrics=['accuracy'])
+cpfile=r'cifar10.hdf5'
+cb_checkpoint=keras.callbacks.ModelCheckpoint(cpfile,monitor='val_acc',verbose=1,save_best_only=True,mode='max')
+model.summary()
+model.fit(xtrain,ytrain_norm,epochs=5,callbacks=[cb_checkpoint],validation_split=0.2,)
+
+
+#prediction
+y_train_pred=model.predict(xtrain)
+y_train_pred=np.argmax(y_train_pred,axis=1)
+print(y_train_pred)
+
+y_test_pred=model.predict(xtest)
+y_test_pred=np.argmax(y_test_pred,axis=1)
+print(y_test_pred)
+
+
+train_acc=scoring(lb.inverse_transform(y_train_pred),lb.inverse_transform(ytrain))
+test_acc=scoring(lb.inverse_transform(y_test_pred),lb.inverse_transform(ytest))
+
+
+print(f'train accuracy: {train_acc} test accuracy: {test_acc}')
+
+
+def plot_cm(cm,text):
+    class_names=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    plot_confusion_matrix(conf_mat=cm,figsize=(8,8),colorbar=True,cmap='Greens',show_absolute=False,show_normed=True)
+    tick_marks=np.arange(len(class_names))
+    plt.xticks(tick_marks,class_names,rotation=45)
+    plt.yticks(tick_marks,class_names)
+    plt.title(text)
+    plt.show()
+
+
+    return
+
+train_cm=confusion_matrix(lb.inverse_transform(ytrain),lb.inverse_transform(y_train_pred))
+test_cm=confusion_matrix(lb.inverse_transform(ytest),lb.inverse_transform(y_test_pred))
+
+# plot_cm(train_cm,'Confusion matrix train data')
+# plot_cm(test_cm,'Confusion matrix test data')
+
+
+
+#dense layer visualization
+
+#index of categories for our model
+classes=lb.inverse_transform(np.arange(10))
+print(classes)
+# img_path=os.path.join('datasets/cifar10',f'{6}.png')
+ship_img = utils.load_img('datasets/cifar10/'+f'{6}.png')
+plt.imshow(ship_img)
+plt.show()
+
+
+#predicting the probility for each of the class for the image
+ship_prob=model.predict(xtest[6:7]).ravel().copy()
+pd.Series(ship_prob,index=classes).plot.barh()
+plt.show()
+
+
+#
+
+
+
 
 
